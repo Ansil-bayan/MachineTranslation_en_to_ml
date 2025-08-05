b01f3a4c72f99915dfeb74db7f55d0450d1d155c
Ansil Bayan <ansilbayan5@gmail.com>
2025-08-06 03:14:08 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,19 +1,18 @@
 import json
 
-# Ruta del notebook 
-ruta = "/MachineTranslation_en_to_ml.ipynb"  # cambia esto
+nb = "/MachineTranslation_en_to_ml.ipynb"  
 
-# Cargar el notebook como JSON
+# load nb as JSON
 with open(ruta, "r", encoding="utf-8") as f:
     data = json.load(f)
 
-# Limpia los widgets si están dañados
+# Clean the widgets if they are damaged
 if "widgets" in data.get("metadata", {}):
-    print("Corrigiendo metadata.widgets...")
+    print("Correcting metadata.widgets")
     del data["metadata"]["widgets"]
 
-# Guardar el notebook limpio
-with open(ruta, "w", encoding="utf-8") as f:
+# Save the clean nb
+with open(nb, "w", encoding="utf-8") as f:
     json.dump(data, f, indent=1)
 
-print("Notebook .")
+print("Notebook cleaned.")
